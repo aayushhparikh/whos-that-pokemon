def56e4400f10f1ac6a952cd3ded0bfa3ecc7cd9
aayushparikh <thedude5190@gmail.com>
2025-03-17 16:59:36 -0400

    initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import Title from "./Components/Title";
+
+export default function App() {
+    return (
+        <div>
+            <Title />
+        </div>
+    );
+  }  
---
+++ frontend/src/Components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+      <div className="flex items-center justify-center h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold text-blue-600">Who's That Pokémon?</h1>
+      </div>
+    );
+  }  
